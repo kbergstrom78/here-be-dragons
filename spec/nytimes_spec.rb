ee2f2e49c6ee481fbecc265c6d3f6d0e5a113c3c
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-20 15:50:26 -0600

    Add passing test for stories/politics

--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -29,8 +29,9 @@ class Nytimes
       expect(result.count).to eq(44)
     end
 
-    xit 'can get all stories with subsection of politics' do
+    it 'can get all stories with subsection of politics' do
       #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
+      result = @hash[:results].select { |result| result[:subsection] == "Politics" }
 
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(6)
